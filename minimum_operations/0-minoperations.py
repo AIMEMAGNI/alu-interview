621d8d0585f4ddb7d1d9058aadeff29dc9f9fd03
root <you@example.com>
2023-09-17 08:42:59 +0000

    done

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
+"""Minimum Operations"""
+import math
 
-"""
-This script provides a function to calculate the fewest number of operations
-needed to result in exactly n H characters in a text file using the "Copy All"
-and "Paste" operations.
-"""
 
 def minOperations(n):
-    """
-    Calculate the fewest number of operations needed to result in exactly n H characters in a text file.
-
-    Parameters:
-    n (int): The desired number of H characters.
-
-    Returns:
-    int: The minimum number of operations required to achieve n H characters. Returns 0 if n is impossible to achieve.
-    """
-
-    # If n is 1, it's impossible to achieve more than 1 H.
-    if n == 1:
+    """Minimum Operations"""
+    if n <= 1:
         return 0
-    
-    # Initialize a list to keep track of minimum operations for each position.
-    dp = [0] + [float('inf')] * n
-    
-    for i in range(2, n+1):
-        for j in range(2, i+1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i // j))
-    
-    return dp[n] if dp[n] != float('inf') else 0
 
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+
+    return operations
